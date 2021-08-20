4813b5654d64b02a423f84afbd6830a4829f0f31
Yonas <yonasfiseha3655@gmail.com>
2021-08-20 08:37:39 +0300

    Structured code

--- two_sum.py
+++ two_sum.py
@@ -1,12 +1,12 @@
 # https://leetcode.com/problems/two-sum/
 
-class Solution(object):
-    def twoSum(self, nums, target):
-      
-       
-        ln= len(nums)
-        for i in range(ln):
-            for j in range(i+1 , ln):
-                if nums[i] + nums[j] == target:
-                 return [i, j]
-     
+def twoSum(nums, target):
+    
+    
+    ln= len(nums)
+    for i in range(ln):
+        for j in range(i+1 , ln):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+    
+print(twoSum([2,7,11,15], 9))
