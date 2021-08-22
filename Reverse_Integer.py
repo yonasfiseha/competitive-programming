9fc0cc0eaaa70d793afabd364a1b8787e718721a
Yonas <yonasfiseha3655@gmail.com>
2021-08-22 21:23:38 +0300

    structured code

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -5,13 +5,17 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
+        isnegative = x < 0
         reverse_num = 0
+        x = abs(x)
         while(x > 0):
             reminder = x % 10 
             reverse_num = (reverse_num *10) + reminder
             x = x//10
+        if isnegative:
+            reverse_num = reverse_num *(-1)
         return reverse_num   
-            
+             
             
 solution = Solution()
-print(solution.reverse(123))
+print(solution.reverse(-123))
