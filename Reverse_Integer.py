78c9426199aa079330a6ccd0f96de724c1b0d3d3
Yonas <yonasfiseha3655@gmail.com>
2021-08-23 01:53:07 +0300

    structured code

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,21 +1,20 @@
 class Solution(object):
-   
-    def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        isnegative = x < 0
-        reverse_num = 0
-        x = abs(x)
-        while(x > 0):
-            reminder = x % 10 
-            reverse_num = (reverse_num *10) + reminder
-            x = x//10
-        if isnegative:
-            reverse_num = reverse_num *(-1)
-        return reverse_num   
-             
-            
+
+      def reverse(self, x):
+             isnegative = x< 0
+             reverse_num = 0
+             x = abs(x)
+             while (x > 0):
+                    reminder = x % 10
+                    reverse_num = (reverse_num * 10) + reminder
+                    x = x // 10
+                    if (reverse_num >= 2147483647 or reverse_num <= -2147483648 ):
+                       reverse_num = 0
+
+             if isnegative:
+                 reverse_num = reverse_num *(-1)
+             return reverse_num
+
+
 solution = Solution()
-print(solution.reverse(-123))
+print(solution.reverse(123))
