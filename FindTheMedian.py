831a8339c7db34b3269ebaa50556db8d8a508f87
yonasfiseha <yonasfiseha3655@gmail.com>
2021-09-01 22:36:52 +0300

    solve  https://www.hackerrank.com/challenges/find-the-median/problem

--- FindTheMedian.py
+++ FindTheMedian.py
@@ -1,10 +1,4 @@
 #https://www.hackerrank.com/challenges/find-the-median/problem
-import math
-import os
-import random
-import re
-import sys
-
 
 nums = [0, 1, 2, 4, 6, 5, 3]
 ln = len(nums)
@@ -17,26 +11,13 @@ def Median(nums):
     nums.sort()
     if ln % 2 == 0:
         val = ln // 2
-        midian_value = nums[val]
+        median_value = nums[val]
     elif ln % 2 != 0:
         val2 = (ln - 1) // 2
-        midian_value = nums[val2]
-    return midian_value
+        median_value = nums[val2]
+    return median_value
 
 
 print(Median(nums))
 
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ln = int(input().strip())
-
-    nums = list(map(int, input().rstrip().split()))
-
-    result = Median(nums)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
 
