b056e4b1f5a75af0fb33e02a8b98cc2b6e4f3da6
yonasfiseha <yonasfiseha3655@gmail.com>
2021-09-20 21:18:30 +0300

    solve https://leetcode.com/problems/h-index/submissions/

---
+++ H_index.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def hIndex(self, citations):
+        ln =len(citations)
+        citations.sort()
+        for i, n in enumerate(citations):
+            h_index = ln - i
+            if h_index <= n:
+                return h_index
+           
+        return 0
+Solution =Solution()
+print('H-index is: ', Solution.hIndex([3,0,6,1,5]))
