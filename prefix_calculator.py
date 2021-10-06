088424a53848fe86b74a8f3b4cf51d457b07f452
yonasfiseha <yonasfiseha3655@gmail.com>
2021-10-06 15:55:55 +0300

    solve prefix calculator

---
+++ prefix_calculator.py
@@ -0,0 +1,28 @@
+# prefix calculator
+# prefix calculator
+digits = '0123456789'
+stack = []
+
+
+def prefixCalculator(expression):
+    for i in expression[::-1]:
+        if i in digits:
+            stack.append(int(i))
+        else:
+            d1 = stack.pop()
+            d2 = stack.pop()
+            if i == '+':
+                stack.append(d1 + d2)
+            elif i == '-':
+                stack.append(d1 - d2)
+
+            elif i == '*':
+                stack.append(d1 * d2)
+
+            elif i == '/':
+                stack.append(d1 / d2)
+
+    return stack.pop()
+
+
+print(prefixCalculator('+5*24'))
